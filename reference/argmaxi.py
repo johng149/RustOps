c2caef610ab0675642c01cbfffd5088b5eb46419
johng149 <johnguo149@gmail.com>
2025-04-13 13:06:24 -0700

    Update how these files are generated

---
+++ reference/argmaxi.py
@@ -0,0 +1,33 @@
+import torch
+from util.save_reference import save_reference
+from typing import Tuple, List, Iterable
+from maxi import create_tensors as create_tensors_maxi
+
+def create_tensors(
+        shape: Tuple[int, ...] | List[int] | Iterable[int],
+        eps = 1e-8,
+        dtype: torch.dtype = torch.float32,
+        dir: str = "data",
+        name: str = "argmaxi",
+):
+     x, max_indices, blank, gathered, scattered = create_tensors_maxi(shape, dtype)
+     maxied = torch.abs(scattered)
+     factors, _ = torch.max(maxied, dim=-1)
+     factors = factors - eps
+     result = maxied / factors.unsqueeze(-1)
+     return x, result
+
+def create_argmaxi(
+        shape: Tuple[int, ...] | List[int] | Iterable[int],
+        eps = 1e-8,
+        dtype: torch.dtype = torch.float32,
+        dir: str = "data",
+        name: str = "argmaxi",
+):
+    x, result = create_tensors(shape, eps, dtype)
+    save_reference(x, dir, f"{name}_x")
+    save_reference(result, dir, f"{name}_result_{eps}")
+
+if __name__ == "__main__":
+    # batch size, nodes, mems
+    create_argmaxi((6, 7, 8), eps=1e-8, dtype=torch.float32, dir="data", name="argmaxi")
--- reference/maxi.py
+++ reference/maxi.py
@@ -2,17 +2,22 @@ import torch
 from util.save_reference import save_reference
 from typing import Tuple, List, Iterable
 
+
+def create_tensors(shape: Tuple[int, ...] | List[int] | Iterable[int], dtype: torch.dtype = torch.float32):
+    x = torch.rand(shape, dtype=dtype)
+    max_indices = torch.argmax(x, dim=-1, keepdim=True)
+    blank = torch.zeros_like(x)
+    gathered = torch.gather(x, -1, max_indices)
+    scattered = torch.scatter(blank, -1, max_indices, gathered)
+    return x, max_indices, blank, gathered, scattered
+
 def create_maxi(
         shape: Tuple[int, ...] | List[int] | Iterable[int],
         dtype: torch.dtype = torch.float32,
         dir: str = "data",
         name: str = "maxi",
 ):
-    x = torch.rand(shape, dtype=dtype)
-    max_indices = torch.argmax(x, dim=-1, keepdim=True)
-    blank = torch.zeros_like(x)
-    gathered = torch.gather(x, -1, max_indices)
-    scattered = torch.scatter(blank, -1, max_indices, gathered)
+    x, max_indices, blank, gathered, scattered = create_tensors(shape, dtype)
     save_reference(x, dir, f"{name}_x")
     save_reference(max_indices, dir, f"{name}_max_indices")
     save_reference(blank, dir, f"{name}_blank")
