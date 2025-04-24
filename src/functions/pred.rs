d0784846d1aecfb5352d7cd304f5e2e3d2d5ef1b
johng149 <johnguo149@gmail.com>
2025-04-24 16:11:04 -0700

    Make pred borrow variables

--- src/functions/pred.rs
+++ src/functions/pred.rs
@@ -4,7 +4,7 @@ use super::einsum::einsum_ndarray_dyn;
 use super::reshape::reshape;
 use super::unsqueeze::unsqueeze;
 
-pub fn pred<T>(parent_down_prop: ArrayD<T>, parent_mem_matrix: ArrayD<T>) -> ArrayD<T>
+pub fn pred<T>(parent_down_prop: &ArrayD<T>, parent_mem_matrix: ArrayD<T>) -> ArrayD<T>
 where
     T: NdFloat,
 {
@@ -17,7 +17,7 @@ where
     let (nodes, children_per_node, memories, dim) =
         (mm_shape[0], mm_shape[1], mm_shape[2], mm_shape[3]);
     let batch_size = parent_down_prop.shape()[0];
-    let prediction = einsum_ndarray_dyn("ncmd,bnm->bncd", &[&mm, &parent_down_prop]).unwrap();
+    let prediction = einsum_ndarray_dyn("ncmd,bnm->bncd", &[&mm, parent_down_prop]).unwrap();
     println!("prediction shape: {:?}", prediction.shape());
     println!(
         "batch_size: {:?}, nodes: {:?}, children_per_node: {:?}, dim: {:?}",
